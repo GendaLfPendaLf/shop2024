b9e8e0cd59cbf26437709701a069db74046b3fde
GendaLfPendaLf <152419495+GendaLfPendaLf@users.noreply.github.com>
2024-05-27 19:52:38 +0300

    писание изменений

--- src/main/java/ru/shop2024/product/Product.java
+++ src/main/java/ru/shop2024/product/Product.java
@@ -4,19 +4,33 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.UUID;
-
-@Data
 @Entity
+@Data
+
 public class Product {
     @Id
+
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false)
     private String brand;
+
+    @Column(nullable = false)
     private String type;
 
-    @Column(precision = 10, scale = 2)
+    @Column(nullable = false, precision = 10, scale = 2)
     private Double price;
+
+    @Column(nullable = false)
     private Long count;
+
+    @Column(nullable = false)
     private String description;
+
+    // Конструктор без аргументов (необходим для Hibernate)
+    public Product() {
+    }
+
+    // Другие конструкторы, геттеры и сеттеры, если необходимо
 }
